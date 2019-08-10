8b82659e15030eacc6e86fa195a6a0baaa4387ef
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-08-10 14:35:40 -0400

    Create u32_to_be_bytes function

--- src/chunks.rs
+++ src/chunks.rs
@@ -431,3 +431,10 @@ impl std::default::Default for AncillaryChunks {
         AncillaryChunks::new()
     }
 }
+
+fn u32_to_be_bytes(num: u32) -> [u8; 4] {
+    #[allow(unsafe_code)]
+    unsafe {
+        std::mem::transmute::<u32, [u8; 4]>(num.to_be())
+    }
+}
