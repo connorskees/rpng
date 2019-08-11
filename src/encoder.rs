4fc1af5f4f8a324f65921269153b1e03a531eb29
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-08-11 16:09:21 -0400

    Add encoder MVP

---
+++ src/encoder.rs
@@ -0,0 +1,24 @@
+#![allow(dead_code, unused_must_use, unused_variables)]
+
+use std::io::{Write, BufWriter};
+use std::path::Path;
+use std::fs::File;
+
+use crate::errors::PNGDecodingError;
+use crate::png::PNG;
+use crate::common::{HEADER, IEND};
+use crate::chunks::Chunk;
+
+pub fn save<S: AsRef<Path>>(png: PNG, file_path: S) -> Result<(), PNGDecodingError> {
+    let buffer = &mut BufWriter::new(File::create(file_path)?);
+    write(png, buffer);
+    Ok(())
+}
+
+pub fn write<T: Write>(png: PNG, buffer: &mut BufWriter<T>) -> Result<(), PNGDecodingError> {
+    buffer.write_all(&HEADER)?;
+    buffer.write_all(&[0u8, 0, 0, 13])?;
+    buffer.write_all(&png.ihdr.as_bytes())?;
+    buffer.write_all(&IEND)?;
+    Ok(())
+}
