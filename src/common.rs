36d9b48c30c8d3cdb69637f9fbd4ccd33449a0a5
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-08-11 20:43:56 -0400

    Document common constants

--- src/common.rs
+++ src/common.rs
@@ -2,7 +2,9 @@ use std::ops::Index;
 
 use crate::errors::MetadataError;
 
+/// The PNG header. In ascii, it can be represented as \x{89}PNG\r\n\x{1a}\n
 pub const HEADER: [u8; 8] = [137u8, 80, 78, 71, 13, 10, 26, 10];
+/// The IEND chunk. It always has a length of 0, and so it is always the same between PNGs
 pub const IEND: [u8; 12] = [0u8, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130];
 
 /// Number of bits per color channel
